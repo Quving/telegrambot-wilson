82fb707f8aace16946c757eb1b8275692ffbe598
Vinh Ngu <vinh-ngu@hotmail.com>
2020-04-15 16:53:18 +0200

    Refactor code.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from config import Config
 updater = Updater(token=Config.BOT_TOKEN)
 dispatcher = updater.dispatcher
 
-chatbothandler = {}
+sessions = {}
 
 # Phrases
 HELP = Config.PHRASES['HELP']
@@ -22,56 +22,54 @@ def start(bot, update):
     bot.sendMessage(update.message.chat_id, INTRODUCTION)
 
 
-def resetWilson(bot, update):
-    if str(update.message.chat_id) in chatbothandler.keys():
+def reset_chatbot_instance(bot, update):
+    if str(update.message.chat_id) in sessions.keys():
         bot.sendMessage(update.message.chat_id, "(Wilson dieded)")
-        chatbothandler[str(update.message.chat_id)].resetChatbot()
+        sessions[str(update.message.chat_id)].resetChatbot()
         bot.sendMessage(update.message.chat_id, "(Wilson respawned")
         bot.sendMessage(update.message.chat_id, "... warum?")
 
 
-def killWilson(bot, update):
-    chatbothandler[str(update.message.chat_id)].killChatbot()
-    chatbothandler.pop(str(update.message.chat_id))
+def kill_chatbot_instance(bot, update):
+    sessions[str(update.message.chat_id)].killChatbot()
+    sessions.pop(str(update.message.chat_id))
     bot.sendMessage(update.message.chat_id, GOODBYE)
 
 
-def startWilson(bot, update):
+def start_chatbot(bot, update):
     newUser = {str(update.message.chat_id): Chatbot()}
-    chatbothandler.update(newUser)
+    sessions.update(newUser)
     bot.sendMessage(update.message.chat_id, GREETING)
 
 
 def chat(bot, update):
-    if (str(update.message.chat_id) in chatbothandler.keys() and
-            chatbothandler[str(update.message.chat_id)].isOn()):
-        reply = chatbothandler[
+    if (str(update.message.chat_id) in sessions.keys() and
+            sessions[str(update.message.chat_id)].isOn()):
+        reply = sessions[
             str(update.message.chat_id)].talk(update.message.text)
         bot.sendMessage(update.message.chat_id, reply)
 
 
-def userhelp(bot, update):
-    bot.sendMessage(update.message.chat_id, "Rufe mich mit 'Hey Wilson'. " +
-                    "Dann antworte ich auch auf deine Nachrichten. " +
-                    "Mit 'Bye Wilson' bin ich wieder afk :D")
+def help(bot, update):
+    bot.sendMessage(update.message.chat_id, HELP)
 
 
-def msgParser(bot, update):
+def parse_message(bot, update):
     dictionary_switch = {
         update.message.text.lower(): chat,
-        "hey wilson": startWilson,
-        "bye wilson": killWilson,
-        "stirb wilson": resetWilson
+        "hey wilson": start_chatbot,
+        "bye wilson": kill_chatbot_instance,
+        "stirb wilson": reset_chatbot_instance
     }
 
     dictionary_switch[update.message.text.lower()](bot, update)
 
 
 # Create Handlers.
-help_handler = CommandHandler("help", userhelp)
+help_handler = CommandHandler("help", help)
 start_handler = CommandHandler("start", start)
-reset_handler = CommandHandler("resetWilson", resetWilson)
-msg_handler = MessageHandler([Filters.text], msgParser)
+reset_handler = CommandHandler("reset", reset_chatbot_instance)
+msg_handler = MessageHandler([Filters.text], parse_message)
 
 # "Add Listeners"
 dispatcher.add_handler(help_handler)
